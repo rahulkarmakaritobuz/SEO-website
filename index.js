d7d7dd75640a730b720f9ce4e5a024a835f80641
rahulkarmakaritobuz <rahul.karmakar@itobuz.com>
2023-02-16 14:31:53 +0530

    after responsive & form validation

---
+++ index.js
@@ -0,0 +1,54 @@
+let validateEmail=(mail) =>{
+    if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+     {
+       return (true)
+     }
+       alert("You have entered an invalid email address!")
+       return (false)
+   }
+
+let formBoxSubmit = () =>{
+    const name=document.getElementById('formBox-name').value.trim();
+    const email=document.getElementById('formBox-email').value.trim();
+    let nameStatus=true;
+    if(name==="" || name===null){
+        alert("Please provide a name");
+        nameStatus=false;
+    }
+    console.log(email);
+    let emailStatus=validateEmail(email);
+    console.log(emailStatus);
+    if(nameStatus===true && emailStatus===true){
+        let status=document.getElementById('suscribe');
+        status.innerHTML="Your data successfully submitted!";
+
+        setTimeout(function (){
+            status.innerHTML="";
+        },5000);
+    }
+}
+
+let speedCheckSubmit = ()=>{
+    const link=document.getElementById('speedCheck-url').value.trim();
+    const email=document.getElementById('speedCheck-email').value.trim();
+    // console.log(new URL(link));
+    let linkDetails;
+    try{
+        linkDetails=new URL(link);
+    }
+    catch(erron){
+        alert("Invalid URL!");
+    }
+    let emailStatus=validateEmail(email);
+    if(linkDetails!==undefined && emailStatus===true){
+        let status=document.getElementById('submitStatus');
+        status.innerHTML="Your data successfully submitted!";
+
+        setTimeout(function (){
+            status.innerHTML="";
+        },5000);
+    }
+
+    
+        
+}
